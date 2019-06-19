753c4d8f8216ce6b38d69fc001ec3fbe788282dc
wissal <wissal.el@gmail.com>
2019-06-19 11:46:08 -0700

    Make PaletteList responsive

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,3 +1,5 @@
+import sizes from './sizes';
+
 export default {
     root: {
         backgroundColor: 'blue',
@@ -12,6 +14,9 @@ export default {
         alignItems: 'flex-start',
         flexDirection: 'column',
         flexWrap: 'wrap',
+        [sizes.down('xl')]: {
+            width: '80%'
+        }
     },
     nav: {
         display: 'flex',
@@ -28,6 +33,13 @@ export default {
         width: '100%',
         display: 'grid',
         gridTemplateColumns: 'repeat(3, 30%)',
-        gridGap: '5%'
+        gridGap: '2.5rem',
+        [sizes.down('md')]: {
+            gridTemplateColumns: 'repeat(2, 50%)'
+        },
+        [sizes.down('xs')]: {
+            gridTemplateColumns: 'repeat(1, 100%)',
+            gridGap: '1.5rem'
+        }
     }
 };
